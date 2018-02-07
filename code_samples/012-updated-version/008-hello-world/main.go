702a40252590bdfa2121d87ec5560fc93c01ec92
Todd McLeod <tuddleymc@gmail.com>
2018-02-07 15:51:53 -0800

    you're doing great

---
+++ code_samples/012-updated-version/008-hello-world/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello everyone, this is the most awesome class in the entire world for having fun and learning the GO programming language.")
+
+	foo()
+
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo.")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
+}
+
+// control flow:
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
+
