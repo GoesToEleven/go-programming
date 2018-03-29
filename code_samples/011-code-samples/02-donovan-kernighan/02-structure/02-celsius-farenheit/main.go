f27a77ae77cb565f239dcc62b6924073e6332913
Todd McLeod <tuddleymc@gmail.com>
2018-03-29 15:04:24 -0700

    you're doing great

--- code_samples/011-code-samples/02-donovan-kernighan/02-structure/02-celsius-farenheit/main.go
+++ code_samples/011-code-samples/02-donovan-kernighan/02-structure/02-celsius-farenheit/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/GoesToEleven/go-programming/code_samples/011-code-samples/02-donovan-kernighan/02-structure/02-celsius-farenheit/tempconv"
+)
 
+func main() {
+	fmt.Printf("Fahrenheit freezing %g\n", tempconv.CToF(tempconv.FreezingC))
+	fmt.Printf("Fahrenheit boiling %g\n", tempconv.CToF(tempconv.BoilingC))
 }
