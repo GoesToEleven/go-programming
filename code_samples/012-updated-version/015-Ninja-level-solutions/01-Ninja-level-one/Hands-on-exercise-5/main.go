becbd41d8e4269229232b18e2bccf4749d6f8074
xn0px90 <xn0px90@gmail.com>
2020-11-15 11:32:11 +0000

    Ninja level solution move

---
+++ code_samples/012-updated-version/015-Ninja-level-solutions/01-Ninja-level-one/Hands-on-exercise-5/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type hotdog int
+
+var x hotdog
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
+}
